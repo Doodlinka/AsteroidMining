29fb12b1e0b0741331f0a5cade5cf911b999182a
Doodlinka <oleshko.darya@gmail.com>
2023-09-18 18:22:49 +0300

    asteroid init i guess

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Asteroid : MonoBehaviour
+public class Asteroid : MonoBehaviour, IDamageable
 {
     private Rigidbody2D rb2d;
 
@@ -20,25 +20,35 @@ public class Asteroid : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        // TODO: unhardcode sizes and speed
-        Size = Random.Range(1, 4);
-        rb2d.velocity = Random.insideUnitCircle.normalized * Random.Range(2f, 8f);
+        Init(Random.Range(1, 4), Random.insideUnitCircle.normalized * Random.Range(2f, 8f));
     }
 
-    public void TakeDamage() {
-        health--;
+    public void TakeDamage(int damage = 1) {
+        health -= damage;
         if (health <= 0) {
             Die();
         }
     }
 
-    private void Die() {
+    public void Die() {
         if (Size > 1) {
             Size--;
-            Instantiate(gameObject);
+            // TODO: change velocity on split and maybe shift the init burden on the spawner
+            Instantiate(gameObject).GetComponent<Asteroid>().Init(Size - 1, rb2d.velocity);
         }
         else {
             Destroy(gameObject);
+        }
+    }
+
+    public void Init(int _size, Vector2 velocity) {
+        Size = _size;
+        rb2d.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.TryGetComponent<IDamageable>(out IDamageable d)) {
+            d.TakeDamage(size);
         }
     }
 }
